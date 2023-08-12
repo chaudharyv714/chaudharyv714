13c3e9241b897ee2c3ded57835b99b3ffd3ac30b
Vipin Chaudhary <chaudharyv714@gmail.com>
2023-08-13 01:59:59 +0530

    netlify chnages

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -5,8 +5,8 @@ function contact() {
         <div id="contact">
             <h2>Contact Me: <span className="line">qwertyuiop</span> </h2>
             <div id="contactslist">
-                <a href="#"> <i className="fas fa-envelope-open-text"></i>  chaudharyv714@gmail.com</a>
-                <a href="#"> <i className="fab fa-facebook"></i>  +91 7290937835</a>
+                <a href="mailto:chaudharyv714@gmail.com"> <i className="fas fa-envelope-open-text"></i>  chaudharyv714@gmail.com</a>
+                <a href="tel:+917290937835"> <i className="fab fa-facebook"></i>  +91 7290937835</a>
                 <a href="https://www.linkedin.com/in/vipin-chaudhary-1a1852190/"> <i className="fab fa-linkedin-in"></i>  Vipin Chaudhary</a>
             </div>
         </div>);
